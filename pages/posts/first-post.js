1d2519c45e09a995550ff6a3b52f2c4b2cf70c01
miguelfranken <miguel.franken@rwth-aachen.de>
2022-07-25 12:55:59 +0200

    Add optimized image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ProfileImage from "./profile-image";
 
 export default function FirstPost() {
   return (
@@ -9,6 +10,7 @@ export default function FirstPost() {
           <a className="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Back to home</a>
         </Link>
       </h2>
+      <ProfileImage />
     </>
   )
 }
