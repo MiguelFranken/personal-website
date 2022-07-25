77865dd7a292d5c094cba44cdc54790fcaa46a24
miguelfranken <miguel.franken@rwth-aachen.de>
2022-07-25 15:17:43 +0200

    Add toggle button and extract css post module

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import { CounterContextProvider } from "./store";
 
+// TODO
+// - State Management with either redux or zustand
 function MyApp({ Component, pageProps }) {
   return (
     <CounterContextProvider>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,11 +2,14 @@ import Link from "next/link";
 import ProfileImage from "./profile-image";
 import Head from "next/head";
 import Layout from "@/components/layout";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {CounterContext} from "../store";
+import styles from './post.module.css';
+import cn from 'classnames';
 
 export default function FirstPost() {
   const [state, dispatch] = useContext(CounterContext);
+  const [isActive, setActive] = useState(false);
 
   return (
     <Layout>
@@ -21,21 +24,36 @@ export default function FirstPost() {
         </Link>
       </h2>
       <ProfileImage />
+
       <div>
         <button
           type="button"
-          className="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className={styles.btn}
           onClick={() => dispatch({ type: 'increment' })}
         >
           Increment
         </button>
         <p>You clicked {state.count} times.</p>
+      </div>
 
-        <style jsx>{`
-          p {
-            color: red;
+      <div>
+        <button
+          type="button"
+          className={styles.btn}
+          onClick={() => setActive(!isActive)}
+        >
+          Toggle Active State
+        </button>
+        <p
+          className={
+            cn({
+              [styles.active]: isActive,
+              [styles.inactive]: !isActive,
+            })
           }
-        `}</style>
+        >
+          { isActive ? 'Active' : 'Not Active' }
+        </p>
       </div>
     </Layout>
   )
