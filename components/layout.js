07a89748429f2e2a43fdb367c26bf74ec4e61e30
miguelfranken <miguel.franken@rwth-aachen.de>
2022-07-25 13:32:33 +0200

    Polish layout component

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,82 @@
 import styles from "./layout.module.css";
+import Head from "next/head";
+import Link from "next/link";
+
+export const siteTitle = 'Next.js Sample Website';
+const navigation = [
+  { name: 'Posts', href: '/posts/first-post' },
+];
 
 export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>
+  return (
+    <>
+      <Head>
+        <title>Awesome Title</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <header className="bg-indigo-600">
+        <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+          <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+            <div className="flex items-center">
+              <a href="#">
+                <span className="sr-only">Workflow</span>
+                <img
+                  className="h-10 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                  alt=""
+                />
+              </a>
+              <div className="hidden ml-10 space-x-8 lg:block">
+                {navigation.map((link) => (
+                  <Link key={link.name} href={link.href}>
+                    <a className="text-base font-medium text-white hover:text-indigo-50">{link.name}</a>
+                  </Link>
+                ))}
+              </div>
+            </div>
+            <div className="ml-10 space-x-4">
+              <a
+                href="#"
+                className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+              >
+                Sign in
+              </a>
+              <a
+                href="#"
+                className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
+              >
+                Sign up
+              </a>
+            </div>
+          </div>
+          <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+            {navigation.map((link) => (
+              <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+                {link.name}
+              </a>
+            ))}
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <div className={styles.container}>
+          {children}
+        </div>
+      </main>
+    </>
+  )
 }
