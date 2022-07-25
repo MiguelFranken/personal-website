eb71b71d3ed6248a5872780a554a6c35ea9f4215
miguelfranken <miguel.franken@rwth-aachen.de>
2022-07-25 13:58:17 +0200

    Add simple hook example

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,8 +2,11 @@ import Link from "next/link";
 import ProfileImage from "./profile-image";
 import Head from "next/head";
 import Layout from "@/components/layout";
+import {useState} from "react";
 
 export default function FirstPost() {
+  const [count, setCount] = useState(0);
+
   return (
     <Layout>
       <Head>
@@ -18,7 +21,14 @@ export default function FirstPost() {
       </h2>
       <ProfileImage />
       <div>
-        <p>Hello World</p>
+        <button
+          type="button"
+          className="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          onClick={() => setCount(count + 1)}
+        >
+          Increment
+        </button>
+        <p>You clicked {count} times.</p>
 
         <style jsx>{`
           p {
