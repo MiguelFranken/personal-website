d3ce5ba0972b53266a724e734223e149461a7b39
miguelfranken <miguel.franken@rwth-aachen.de>
2022-07-26 19:29:17 +0200

    Add pattern

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Layout from "@/components/layout";
 import Card from "@/components/card/card";
 import Input from "@/components/input/input";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { ArticlesContext } from "@/pages/store";
+import CardHeader from "@/components/card/card-header";
 
 export default function Home() {
   const { dispatch } = useContext(ArticlesContext);
@@ -51,6 +52,37 @@ export default function Home() {
             />
           </div>
         </Card>
+
+        <div className="py-8">
+          <div className="bg-white overflow-hidden shadow rounded-lg relative">
+            <div className="text-gray-200">
+              <svg
+                aria-hidden="true"
+                className="h-full w-full absolute border-amber-200"
+              >
+                <defs>
+                  <pattern
+                    id=":R196:"
+                    width="128"
+                    height="128"
+                    patternUnits="userSpaceOnUse"
+                    x="100%"
+                    y="100%"
+                    patternTransform="translate(112 64)"
+                  >
+                    <path
+                      d="M0 128V.5H128"
+                      fill="none"
+                      stroke="currentColor"
+                    ></path>
+                  </pattern>
+                </defs>
+                <rect width="100%" height="100%" fill="url(#:R196:)"></rect>
+              </svg>
+            </div>
+            <div className="px-4 py-5 sm:p-6">Content</div>
+          </div>
+        </div>
       </Layout>
     </>
   );
