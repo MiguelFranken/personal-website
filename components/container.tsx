db0451e600c8eac8d39c4a1a128daa0a167b1c22
miguelfranken <miguel.franken@rwth-aachen.de>
2022-08-09 18:12:18 +0200

    Add old container component into _app.tsx
    This component will not be reused. Thus, I decided to move it into _app.tsx where it is used.

--- components/container.tsx
+++ components/container.tsx
@@ -1,15 +1,3 @@
-import styles from "@/styles/container.module.css";
-import classNames from "classnames";
-
 export default function Container(props) {
-  return (
-    <div
-      className={classNames(
-        "bg-clip-border border-l-20 border-t-20 border-gray-900 min-h-screen",
-        styles.bg
-      )}
-    >
-      {props.children}
-    </div>
-  );
+  return <div className="">{props.children}</div>;
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ArticlesContextProvider } from "@/lib/store";
 import React from "react";
-import Container from "@/components/container";
+
+const Container = (props) => {
+  return (
+    <div className="bg-clip-border border-l-20 border-t-20 border-gray-900 min-h-screen bg">
+      {props.children}
+    </div>
+  );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
