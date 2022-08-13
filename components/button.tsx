b8a1bb67a18655be5c476bcb0f2da2d9f7ee1206
miguelfranken <miguel.franken@rwth-aachen.de>
2022-08-13 18:35:42 +0200

    Add article action buttons

--- components/button.tsx
+++ components/button.tsx
@@ -1,9 +1,12 @@
+import { ReactElement } from "react";
+
 export type ButtonProps = {
   href: string;
   text: string;
+  icon: ReactElement;
 };
 
-export default function Button({ href, text }: ButtonProps) {
+export default function Button({ href, text, icon }: ButtonProps) {
   return (
     <a
       href={href}
@@ -11,8 +14,9 @@ export default function Button({ href, text }: ButtonProps) {
     >
       <span className="absolute inset-0 transition-transform translate-x-0 translate-y-0 bg-gradient-to-r from-yellow-300 to-yellow-200 group-hover:translate-y-1.5 group-hover:translate-x-1.5"></span>
 
-      <span className="w-full relative inline-flex justify-center items-center gap-2 px-8 py-3 text-sm font-bold tracking-widest uppercase border-2 border-current">
-        <span>{text}</span>
+      <span className="flex justify-center items-center gap-2 w-full relative inline-block px-8 py-3 text-sm font-bold tracking-widest uppercase border-2 border-current">
+        {icon}
+        {text}
       </span>
     </a>
   );
