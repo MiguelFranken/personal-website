d6961cb9a77a2103207f8cb69844a7be7a06c5c5
miguelfranken <miguel.franken@rwth-aachen.de>
2022-08-16 11:52:48 +0200

    Fix hero action button top margin

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -68,7 +68,7 @@ export default function Hero() {
             find me when I&apos;m not working.
           </p>
           <div className="mt-6 sm:max-w-lg sm:w-full sm:flex">
-            <div className="items-baseline mt-4 sm:mt-0 flex-col md:flex md:flex-row gap-4 space-y-4 lg:space-y-0 md:order-2 w-full lg:w-min">
+            <div className="items-baseline mt-6 sm:mt-0 flex-col md:flex md:flex-row gap-4 space-y-4 lg:space-y-0 md:order-2 w-full lg:w-min">
               {navigation.map((item) => (
                 <div key={item.name}>
                   <a
