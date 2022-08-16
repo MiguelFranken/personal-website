0b285dace973641c35c333ac23e063d4933e7e3a
miguelfranken <miguel.franken@rwth-aachen.de>
2022-08-16 16:02:45 +0200

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,8 +19,32 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
+
         <meta name="theme-color" content="white" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="white" />
+
         <title>Miguel Franken</title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <BorderContainer>
