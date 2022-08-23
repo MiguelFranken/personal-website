b4fe2a5804cfb9d7d21f34d506e62a3861f8f969
miguelfranken <miguel.franken@rwth-aachen.de>
2022-08-24 00:44:57 +0200

    Fix command dependency

--- components/terminal/terminal.tsx
+++ components/terminal/terminal.tsx
@@ -65,7 +65,7 @@ export default function Terminal() {
         await pushToHistory(item);
       },
     }),
-    [pushToHistory]
+    [pushToHistory, resetHistory]
   );
 
   // add initial history
