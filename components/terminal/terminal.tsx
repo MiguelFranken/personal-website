eb46a3c8ad3ce810020cecb966d25afe71aa5d38
miguelfranken <miguel.franken@rwth-aachen.de>
2022-08-24 00:35:00 +0200

    Make terminal header background blurry

--- components/terminal/terminal.tsx
+++ components/terminal/terminal.tsx
@@ -98,7 +98,7 @@ export default function Terminal() {
   );
 
   const TerminalHeader = () => (
-    <div className="shrink-0 grid grid-cols-3">
+    <div className="shrink-0 sticky inset-x-0 top-0 bg-white/50 backdrop-blur-md grid grid-cols-3 px-4 py-3 border-b">
       <div className="flex items-center gap-2">
         <div className="rounded-full bg-red-500 w-3.5 h-3.5"></div>
         <div className="rounded-full bg-yellow-500 w-3.5 h-3.5"></div>
@@ -125,11 +125,11 @@ export default function Terminal() {
     <div
       onClick={focusInput}
       onKeyDown={onKeyDown}
-      className="w-full h-full overflow-scroll focus-within:ring-4 focus-within:ring-yellow-400 font-mono flex flex-col bg-white border-2 border-current px-4 py-3 space-y-6"
+      className="w-full h-full overflow-scroll focus-within:ring-4 focus-within:ring-yellow-400 font-mono flex flex-col bg-white border-2 border-current space-y-3"
     >
       <TerminalHeader />
 
-      <div className="flex-1">
+      <div className="flex-1 px-4 pb-3">
         <TerminalBodyHistory />
         <Term ref={term} executeCommand={executeCommand} />
       </div>
