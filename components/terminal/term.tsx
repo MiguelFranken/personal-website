5b653d620e7e8a91bd21d84d7727750e8cdc9da6
miguelfranken <miguel.franken@rwth-aachen.de>
2022-08-23 21:38:13 +0200

    Refactor term component

---
+++ components/terminal/term.tsx
@@ -0,0 +1,49 @@
+import classNames from "classnames";
+import styles from "@/styles/term.module.css";
+import { forwardRef, KeyboardEventHandler, useState } from "react";
+
+const Term = (props, ref) => {
+  const [value, setValue] = useState("");
+
+  const handleKeyDown: KeyboardEventHandler = (event) => {
+    if (event.key === "Enter") {
+      console.log("Pressed Enter");
+      event.preventDefault();
+    } else if (event.key === "Tab") {
+      console.log("Pressed tab");
+      event.preventDefault();
+    }
+  };
+
+  const changeInput = (value) => {
+    setValue(value.replaceAll(" ", "&nbsp;"));
+  };
+
+  return (
+    <div className="flex">
+      <div className="font-bold">guest@miguel ~ %&nbsp;</div>
+      <span className={classNames(styles.growingInput, "inline-grid group")}>
+        <input
+          ref={ref}
+          onKeyDown={handleKeyDown}
+          type="text"
+          maxLength={20}
+          className="font-bold focus:outline-0 border-none focus:ring-0 p-0 w-full block relative caret-transparent"
+          onChange={(event) => changeInput(event.target.value)}
+        />
+        <span
+          className={classNames(
+            styles.dummy,
+            "pointer-events-none font-bold invisible",
+            "inline-block relative max-w-min",
+            "after:visible after:absolute after:inset-y-0.5 after:-right-3 after:inline-block after:bg-gray-900 after:align-top after:aspect-[9/16]",
+            "group-focus-within:after:animate-none"
+          )}
+          dangerouslySetInnerHTML={{ __html: value }}
+        ></span>
+      </span>
+    </div>
+  );
+};
+
+export default forwardRef<HTMLInputElement>(Term);
